ee31d8bad54ef754f29aa13aa064de7665517154
Ardika Rommy Sanjaya <contact@ardikars.com>
2020-04-09 07:07:45 +0700

    Add jvm.config and rename build script

--- .mvn/install-local.sh
+++ .mvn/install-local.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
-cp .mvn/toolchains.xml ~/.m2
+# Run from root project $ ./.mvn/install-local.sh
 
 export JAVA_HOME_8="/Volumes/Data/Application/jdk8u222-b10/Contents/Home"
 export JAVA_HOME_14="/Volumes/Data/Application/jdk-14.jdk/Contents/Home"
 export JAVA_HOME=${JAVA_HOME_14}
 
-./mvnw clean install -Plegacy-support -Pformat -DskipTests
+./mvnw -t ./mvn/toolchains.xml clean install -Plegacy-support -Pformat -DskipTests
