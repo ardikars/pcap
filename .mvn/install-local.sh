1d6d5a21bce2bf339cca095fd4cb59aec5c6b50d
Ardika Rommy Sanjaya <contact@ardikars.com>
2020-04-21 19:03:15 +0700

    Fix script

--- .mvn/install-local.sh
+++ .mvn/install-local.sh
@@ -6,4 +6,4 @@ export JAVA_HOME_8="/Volumes/Data/Application/jdk8u222-b10/Contents/Home"
 export JAVA_HOME_14="/Volumes/Data/Application/jdk-14.jdk/Contents/Home"
 export JAVA_HOME=${JAVA_HOME_14}
 
-./mvnw -t ./mvn/toolchains.xml clean install -Plegacy-support -Pformat -DskipTests
+./mvnw -t .mvn/toolchains.xml clean install -Plegacy-support -Pformat -DskipTests
