d7161091e8bce087d6281e0d081f7549834dacf0
ardikars <contact@ardikars.com>
2020-08-10 20:48:28 -0700

    Add packet buffer utils

---
+++ api/src/test/java/pcap/api/PacketBufferUtilsTest.java
@@ -0,0 +1,45 @@
+package pcap.api;
+
+import java.nio.ByteBuffer;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.junit.platform.runner.JUnitPlatform;
+import org.junit.runner.RunWith;
+import pcap.common.memory.Memories;
+import pcap.common.memory.Memory;
+import pcap.spi.PacketBuffer;
+
+@RunWith(JUnitPlatform.class)
+public class PacketBufferUtilsTest {
+
+  @Test
+  public void fromDirectByteBufferTest() {
+    ByteBuffer buffer = ByteBuffer.allocateDirect(8);
+    PacketBuffer packetBuffer = PacketBufferUtils.fromDirectByteBuffer(buffer);
+    Assertions.assertNotNull(packetBuffer);
+  }
+
+  @Test
+  public void fromDirectByteBufferNegativeTest() {
+    ByteBuffer buffer = ByteBuffer.allocate(8);
+    Assertions.assertThrows(
+        IllegalArgumentException.class, () -> PacketBufferUtils.fromDirectByteBuffer(buffer));
+  }
+
+  @Test
+  public void fromMemoryTest() {
+    ByteBuffer buffer = ByteBuffer.allocateDirect(8);
+    Memory memory = Memories.wrap(buffer);
+    PacketBuffer packetBuffer = PacketBufferUtils.fromMemory(memory);
+    Assertions.assertNotNull(packetBuffer);
+    memory.release();
+  }
+
+  @Test
+  public void fromMemoryNegativeTest() {
+    ByteBuffer buffer = ByteBuffer.allocate(8);
+    Memory memory = Memories.wrap(buffer);
+    Assertions.assertThrows(
+        IllegalArgumentException.class, () -> PacketBufferUtils.fromMemory(memory));
+  }
+}
